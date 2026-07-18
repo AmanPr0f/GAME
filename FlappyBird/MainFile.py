aaafdc989ba5a8286e4b79f4487f1b4643d33583
aman.pr0f <aman.professional.9634@gmail.com>
2026-07-18 15:37:12 +0530

    commit

--- FlappyBird/MainFile.py
+++ FlappyBird/MainFile.py
@@ -1,4 +1,45 @@
 import pygame
+import random
 
-x = pygame.init()
-gameWindow = pygame.display.set_mode((700, 500))
+pygame.init()
+gameScreen = pygame.display.set_mode((900, 600))
+gameTitle = pygame.display.set_caption("Space Shooter")
+
+#Surface
+block = pygame.Surface((100, 200))
+
+#Player
+player = pygame.image.load('image/player.png').convert_alpha()
+#Star
+star  = pygame.image.load('image/star.png').convert_alpha()
+#Star random
+x = random.randint(0, 900)
+y = random.randint(0, 600)
+
+fps = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+#BAckground
+    gameScreen.fill('gray29')
+#PLayer
+    gameScreen.blit(player, (100, 100))
+#Star
+    gameScreen.blit(star, (x, y))
+
+
+
+
+
+    pygame.display.update()
+   
+    #FPS
+    fps.tick(60)
+
+pygame.quit()            
